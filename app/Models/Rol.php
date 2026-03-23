1cb84ab5fa5e4264c9b469bde50cad39b52d034d
Diego Trigotenco Perez <diegotrigotenco339@gmail.com>
2026-03-23 11:23:04 -0600

    Primer Commit api

---
+++ app/Models/Rol.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model
+{
+    protected $table = 'roles';
+
+    protected $fillable = ['nombre'];
+
+    public $timestamps = false;
+
+    public function usuarios()
+    {
+        return $this->belongsToMany(Usuario::class, 'usuario_rol', 'rol_id', 'usuario_id');
+    }
+}
